60d253a3afb1ab0bb195e42c96c3a430721354a7
Nolhan Colas <nolhancolas@gmail.com>
2026-07-09 09:07:25 +0000

    Add gallery route redirects

---
+++ src/app/client-gallery/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import { redirect } from "next/navigation";
+
+type PageProps = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export default async function ClientGallerySlugRedirectPage({ params }: PageProps) {
+  const { slug } = await params;
+
+  redirect(`/client-albums/${slug}`);
+}
